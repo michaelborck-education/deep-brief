71b63de4e14949850ea0087965770d67ab95a9bd
Michael Borck <michael.borck@curtin.edu.au>
2025-11-12 20:04:39 +0800

    Bump version to 0.3.4

--- src/deep_brief/__init__.py
+++ src/deep_brief/__init__.py
@@ -1,5 +1,5 @@
 """DeepBrief - Video Analysis Application."""
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Michael Borck"
 __description__ = "A video analysis application for presentation feedback"
